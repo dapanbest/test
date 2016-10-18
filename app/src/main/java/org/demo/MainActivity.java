05edd8125b80828e5735893716a5601a9451567f
Peng Yu <wapkpy@qq.com>
2016-10-18 21:07:43 +0800

    commit for upload

--- app/src/main/java/org/demo/MainActivity.java
+++ app/src/main/java/org/demo/MainActivity.java
@@ -2,6 +2,8 @@ package org.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This is a boring message.", Toast.LENGTH_LONG).show();
+            }
+        });
+        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This is a boring message.", Toast.LENGTH_LONG).show();
+            }
+        });
+        findViewById(R.id.btn3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This is a boring message.", Toast.LENGTH_LONG).show();
+            }
+        });
+        findViewById(R.id.btn4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This is a boring message.", Toast.LENGTH_LONG).show();
+            }
+        });
+        findViewById(R.id.btn5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This is a boring message.", Toast.LENGTH_LONG).show();
+            }
+        });
+        findViewById(R.id.btn6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This is a boring message.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
